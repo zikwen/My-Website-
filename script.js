f9bc2409e7d01f308becee74eff8d439249b2f42
zikwen <lordowenzikwen@gmail.com>
2026-01-10 10:11:17 +0100

    Add files via upload
    Signed-off-by: zikwen <lordowenzikwen@gmail.com>

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute('href'))
+      .scrollIntoView({ behavior: 'smooth' });
+  });
+});
